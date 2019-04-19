7b9cd9f567afa55b1aa9e4b1fa0bde87a6214a5a
Rafael Quintanilha <rafaelquintanilha@poli.ufrj.br>
2019-04-19 16:58:30 -0400

    Use env variables

--- src/butter-client.js
+++ src/butter-client.js
@@ -1,6 +1,11 @@
 import Butter from 'buttercms'
-import API_KEY from './apiKey';
 
-const butter = Butter(API_KEY)
+// Import your API KEY and use here
+// import API_KEY from './apiKey';
+
+// I'll use Netlify env variables for the sake of this repo
+const API_KEY = process.env.BUTTER_API_KEY;
+
+const butter = Butter(API_KEY);
 
 export default butter
